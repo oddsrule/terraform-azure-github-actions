7157099b0925af7208f285bb4f1033e1b3f4cf64
Jensen <kirk.jensen@cgi.com>
2020-11-28 13:06:30 -0700

    adding vnet back in - terraform syntax issue resolved

---
+++ terraform/02-vnet.tf
@@ -0,0 +1,9 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${local.service_name}-vnet"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = [ "10.0.0.0/8" ]
+  dns_servers         = [ "8.8.8.8", "8.8.4.4" ]
+  subnet              = []
+  tags                = local.tags
+}
