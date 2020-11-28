a3a6d7ae61b3d86ab788997c80f94a5513549da8
Jensen <kirk.jensen@cgi.com>
2020-11-28 16:02:42 -0700

    updated tf_actions_version in main.yaml

--- terraform/02-vnet.tf
+++ terraform/02-vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "main" {
-  name                = "${local.service_name}-vnetwork"
+  name                = "${local.service_name}-vnet"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   address_space       = [ "10.0.0.0/8" ]
