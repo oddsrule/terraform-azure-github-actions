ee7af1aa009bff84fe6d16648fafa0c828675cdb
Jensen <kirk.jensen@cgi.com>
2020-11-30 20:12:54 -0700

    added backend information to provider and variable tf files - have not updated main.yaml which is my next step

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -16,3 +16,8 @@ locals {
     "Terraform" = "true"
   }
 }
+
+variable "backend_resource_group_name" {}
+variable "backend_storage_account_name" {}
+variable "backend_container_name" {}
+variable "backend_key" {}
